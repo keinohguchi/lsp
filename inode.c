4f65e4903703ca7e35c24a96eeb96f52da00de93
Kei Nohguchi <kei@nohguchi.com>
2018-12-10 09:55:16 -0800

    inode.c: get_inode() example, page 131

---
+++ inode.c
@@ -0,0 +1,39 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+static int get_inode(int fd)
+{
+	struct stat buf;
+	int ret;
+
+	ret = fstat(fd, &buf);
+	if (ret == -1) {
+		perror("fstat");
+		return -1;
+	}
+	return buf.st_ino;
+}
+
+static const char *app = "inode";
+
+int main(int argc, const char *argv[])
+{
+	int fd;
+
+	if (argc < 2) {
+		fprintf(stderr, "usage: %s <filename>\n", app);
+		return 1;
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1) {
+		perror("open");
+		return 1;
+	}
+	if (close(fd) == -1)
+		perror("close");
+	return 0;
+}
