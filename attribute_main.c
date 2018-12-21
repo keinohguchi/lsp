d04f7eae75c8cbadc9e3cb2729da6ffccfd84b68
Kei Nohguchi <kei@nohguchi.com>
2018-12-20 20:29:07 -0800

    attribute: Introduce the sub command approach

--- attribute_main.c
+++ attribute_main.c
@@ -3,52 +3,50 @@
 #include <stdlib.h>
 #include <string.h>
 
-extern char *listattr(const char *path, size_t *len);
-extern int setattr(const char *path, const char *key, const void *buf, size_t len);
-extern char *getattr(const char *path, const char *key, size_t *len);
+static void usage(const char *app)
+{
+	fprintf(stderr,
+		"usage: %s <file> [list|get|set] [key [value]]\n",
+		app);
+	exit(EXIT_SUCCESS);
+}
+
+static int list(const char *path)
+{
+	return 0;
+}
+
+static int get(const char *path, const char *key)
+{
+	return 0;
+}
+
+static int set(const char *path, const char *key, const char *val)
+{
+	return 0;
+}
 
 int main(int argc, char *argv[])
 {
-	const char *key = "user:something";
-	char *val = "something";
-	char *keys, *ptr, *endp;
-	size_t len;
-	int ret;
+	const char *path, *cmd;
+	int ret = 1;
 
-	if (argc < 2) {
-		fprintf(stderr, "usage: %s <file> [<key> [<value>]]\n", argv[0]);
-		return 0;
-	}
-	if (argc > 2)
-		key = argv[2];
-	if (argc > 3) {
-		val = argv[3];
-		len = strlen(val)+1;
-	}
-	keys = listattr(argv[1], &len);
-	if (!keys) {
-		printf("%s: cannot get the attribute list\n", argv[1]);
-		return 1;
-	}
-	for (ptr = val, endp = ptr+len; ptr != endp; ptr += len) {
-		len = printf("%s\n", ptr);
-		if (len < 0) {
-			perror("printf");
-			free(keys);
-			return 1;
-		}
-	}
-	ret = setattr(argv[1], key, val, len);
-	if (ret == -1) {
-		printf("%s: cannot set value=%s for key=%s\n", argv[1], val, key);
-		return 1;
-	}
-	val = getattr(argv[1], key, &len);
-	if (!val) {
-		printf("%s: no value for key=%s\n", argv[1], key);
-		return 1;
-	}
-	printf("%s=%s\n", key, val);
-	free(val);
-	return 0;
+	if (argc < 3)
+		usage(argv[0]);
+	path = argv[1];
+	cmd = argv[2];
+	if (!strncmp(cmd, "list", strlen(cmd)))
+		ret = list(path);
+	else if (!strncmp(cmd, "get", strlen(cmd))) {
+		if (argc < 4)
+			usage(argv[0]);
+		ret = get(path, argv[3]);
+	} else if (!strncmp(cmd, "set", strlen(cmd))) {
+		if (argc < 5)
+			usage(argv[0]);
+		ret = set(path, argv[3], argv[4]);
+	} else
+		usage(argv[0]);
+
+	return ret;
 }
