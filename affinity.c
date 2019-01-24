3886ec304c4a15ce42776ed72ee1f1c8abf1d42a
Kei Nohguchi <kei@nohguchi.com>
2019-01-24 11:21:58 -0800

    affinity: Add command line option to control set and get behavior

--- affinity.c
+++ affinity.c
@@ -1,8 +1,14 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 #include <stdio.h>
-#include <sched.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
+#include <limits.h>
+#include <getopt.h>
+#include <sched.h>
+
+static const char *progname;
 
 static int getaffinity(cpu_set_t *mask)
 {
@@ -40,14 +46,52 @@ int setaffinity(const cpu_set_t *mask)
 	return 0;
 }
 
+static void usage(FILE *stream, int status, const char *const opts)
+{
+	fprintf(stream, "usage: %s [-%s]\n", progname, opts);
+	exit(status);
+}
+
 int main(int argc, char *argv[])
 {
+	const char *opts = "c:hl";
 	cpu_set_t mask;
-	int ret, i;
+	long int cpu = -1;
+	bool list = false;
+	int ret, i, opt;
 
+	progname = argv[0];
+	while ((opt = getopt(argc, argv, opts)) != -1) {
+		switch (opt) {
+		case 'c':
+			cpu = strtol(optarg, NULL, 10);
+			if (cpu < 0 || cpu >= LONG_MAX)
+				usage(stderr, EXIT_FAILURE, opts);
+			break;
+		case 'h':
+			usage(stdout, EXIT_SUCCESS, opts);
+			break;
+		case 'l':
+			list = true;
+			break;
+		case '?':
+		default:
+			usage(stderr, EXIT_FAILURE, opts);
+			break;
+		}
+	}
+
+	CPU_ZERO(&mask);
+	if (cpu != -1) {
+		CPU_SET(cpu, &mask);
+		if (setaffinity(&mask) == -1)
+			return 1;
+		if (!list)
+			return 0;
+	}
 	ret = getaffinity(&mask);
 	if (ret == -1)
-		exit(EXIT_FAILURE);
+		return 1;
 
 	printf("CPU_SETSIZE=%d\n", CPU_SETSIZE);
 	for (i = 0; i < CPU_SETSIZE; i++)
