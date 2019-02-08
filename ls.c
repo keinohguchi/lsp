d6a5ffb4e8f11d10d62f97b6add76fe3c66f70cc
Kei Nohguchi <kei@nohguchi.com>
2019-02-07 20:54:40 -0800

    ls: Support the default listting
    Still WiP.  I need to figure out how to format the multiple contents
    through opendir() and readdir().  `ls` is hard. :)

--- ls.c
+++ ls.c
@@ -2,26 +2,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <unistd.h>
 #include <getopt.h>
 #include <limits.h>
 #include <string.h>
 #include <dirent.h>
 #include <sys/types.h>
+#include <sys/ioctl.h>
 
-static const char *progname;
-
-struct options {
-	bool all;
+/* program context */
+static struct context {
+	const char		*progname;
+	const struct option	lopts[3];
+	const char		*const opts;
+	struct winsize		win;
+	bool			all;
+} ctx = {
+	.lopts = {
+		{"all",		no_argument,	NULL,	'a'},
+		{"help",	no_argument,	NULL,	'h'},
+		{},
+	},
+	.opts	= "ah",
 };
 
-static void usage(FILE *stream, int status, const char *const opts,
-		  const struct option *const lopts)
+static void usage(const struct context *const ctx, FILE *stream, int status)
 {
 	const struct option *o;
 
-	fprintf(stream, "usage: %s [-%s]\n", progname, opts);
+	fprintf(stream, "usage: %s [-%s]\n", ctx->progname, ctx->opts);
 	fprintf(stream, "options:\n");
-	for (o = lopts; o->name; o++) {
+	for (o = ctx->lopts; o->name; o++) {
 		fprintf(stream, "\t--%s,-%c:\t", o->name, o->val);
 		switch (o->val) {
 		case 'a':
@@ -38,10 +49,11 @@ static void usage(FILE *stream, int status, const char *const opts,
 	exit(status);
 }
 
-static int list(const char *const file, const struct options *const o)
+static int list(const struct context *const ctx, const char *const file)
 {
 	char path[PATH_MAX];
 	struct dirent *d;
+	size_t len, total;
 	DIR *dir;
 	int ret;
 
@@ -53,12 +65,25 @@ static int list(const char *const file, const struct options *const o)
 		perror("opendir");
 		return -1;
 	}
+	total = 0;
 	while ((d = readdir(dir)) != NULL) {
-		if (d->d_name[0] == '.' && !o->all)
+		if (d->d_name[0] == '.' && !ctx->all)
 			continue;
-		printf("%s\n", d->d_name);
+		if (total + strlen(d->d_name) + 2 > ctx->win.ws_col-1) {
+			printf("\n");
+			total = 0;
+		}
+		len = printf("%s  ", d->d_name);
+		if (len < 0) {
+			perror("printf");
+			ret = -1;
+			goto out;
+		}
+		total += len;
 	}
+	printf("\n");
 	ret = 0;
+out:
 	if (closedir(dir) == -1) {
 		perror("closedir");
 		return -1;
@@ -68,39 +93,37 @@ static int list(const char *const file, const struct options *const o)
 
 int main(int argc, char *const argv[])
 {
-	const struct option lopts[] = {
-		{"all",		no_argument,	NULL,	'a'},
-		{"help",	no_argument,	NULL,	'h'},
-		{},
-	};
-	const char *const opts = "ah";
-	struct options o;
 	int opt, ret;
 
-	progname = argv[0];
-	memset(&o, 0, sizeof(o));
-	while ((opt = getopt_long(argc, argv, opts, lopts, NULL)) != -1) {
+	ctx.progname = argv[0];
+	while ((opt = getopt_long(argc, argv, ctx.opts, ctx.lopts, NULL)) != -1) {
 		switch (opt) {
 		case 'h':
-			usage(stdout, EXIT_SUCCESS, opts, lopts);
+			usage(&ctx, stdout, EXIT_SUCCESS);
 			break;
 		case 'a':
-			o.all = true;
+			ctx.all = true;
 			break;
 		case '?':
 		default:
-			usage(stderr, EXIT_FAILURE, opts, lopts);
+			usage(&ctx, stderr, EXIT_FAILURE);
 			break;
 		}
 	}
+	/* get the window size */
+	ret = ioctl(STDOUT_FILENO, TIOCGWINSZ, &ctx.win);
+	if (ret == -1) {
+		perror("ioctl");
+		goto out;
+	}
 	/* let's rock */
 	if (optind == argc)
-		ret = list(".", &o);
+		ret = list(&ctx, ".");
 	else
 		while (optind < argc)
-			if ((ret = list(argv[optind++], &o)) == -1)
-				goto done;
-done:
+			if ((ret = list(&ctx, argv[optind++])) == -1)
+				goto out;
+out:
 	if (ret != 0)
 		return 1;
 	return 0;
