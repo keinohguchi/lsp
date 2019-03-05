d61e5728e1f8be734eb1bd2e65fb4dec38ff2150
Kei Nohguchi <kei@nohguchi.com>
2019-03-05 12:19:17 -0800

    find: Add -r for the recursive find

--- find.c
+++ find.c
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <unistd.h>
 #include <getopt.h>
 #include <limits.h>
@@ -13,10 +14,13 @@
 #include <sys/types.h>
 
 static const char *progname;
+static const char *pattern;
+static bool recursive = true;
 static const char *opts = "n:h";
 static const struct option lopts[] = {
 	{"name",	required_argument,	NULL,	'n'},
 	{"help",	no_argument,		NULL,	'h'},
+	{"recursive",	no_argument,		NULL,	'r'},
 	{NULL,		0,			NULL,	0},
 };
 
@@ -29,7 +33,10 @@ static void usage(FILE *stream, int status)
 		fprintf(stream, "\t-%c,--%s:", o->val, o->name);
 		switch (o->val) {
 		case 'n':
-			fprintf(stream, "\tfind specified name\n");
+			fprintf(stream, "\tfind specified pattern\n");
+			break;
+		case 'r':
+			fprintf(stream, "\trecursively find the file\n");
 			break;
 		case 'h':
 			fprintf(stream, "\tdisplay this message and exit\n");
@@ -59,15 +66,29 @@ static const char *pathname(const char *base, const char *file, char *buf, size_
 	}
 }
 
-static int find(const char *restrict path, const char *file, const char *restrict pattern)
+static int pathmatch(const char *restrict path, const char *restrict pattern)
 {
-	char buf[PATH_MAX];
+	char *tmp, *name;
+	int ret;
+	tmp = strdup(path);
+	if (tmp == NULL) {
+		perror("strdup");
+		return -1;
+	}
+	name = basename(tmp);
+	ret = fnmatch(pattern, name, FNM_EXTMATCH);
+	free(tmp);
+	return ret;
+}
+
+static int find(const char *const path)
+{
 	struct dirent *d;
 	struct stat s;
 	DIR *dir;
 	int ret;
 
-	if (!fnmatch(pattern, file, FNM_EXTMATCH))
+	if (!pathmatch(path, pattern))
 		printf("%s\n", path);
 
 	ret = lstat(path, &s);
@@ -85,10 +106,23 @@ static int find(const char *restrict path, const char *file, const char *restric
 		return 0;
 	}
 	while ((d = readdir(dir))) {
+		char *child;
 		if (!strcmp(d->d_name, ".") || !strcmp(d->d_name, ".."))
 			continue;
-		pathname(path, d->d_name, buf, sizeof(buf));
-		if ((ret = find(buf, d->d_name, pattern)))
+		child = malloc(PATH_MAX);
+		if (child == NULL) {
+			perror("malloc");
+			ret = -1;
+			break;
+		}
+		pathname(path, d->d_name, child, PATH_MAX);
+		if (recursive)
+			ret = find(child);
+		else
+			/* thread version will come here */
+			ret = 0;
+		free(child);
+		if (ret)
 			break;
 	}
 	if (closedir(dir))
@@ -98,15 +132,18 @@ static int find(const char *restrict path, const char *file, const char *restric
 
 int main(int argc, char *argv[])
 {
-	const char *pattern = "*";
 	const char *path;
 	int opt;
 
 	progname = argv[0];
+	pattern = "*";
 	while ((opt = getopt_long(argc, argv, opts, lopts, NULL)) != -1)
 		switch (opt) {
 		case 'n':
 			pattern = optarg;
+			break;
+		case 'r':
+			recursive = true;
 			break;
 		case 'h':
 			usage(stdout, EXIT_SUCCESS);
@@ -121,7 +158,7 @@ int main(int argc, char *argv[])
 
 	/* let's roll */
 	path = argv[optind];
-	if (find(path, path, pattern))
+	if (find(path))
 		return 1;
 	return 0;
 }
