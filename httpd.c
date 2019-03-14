129178dd03b0549b8f0346ba893b692a31d83c75
Kei Nohguchi <kei@nohguchi.com>
2019-03-13 21:14:44 -0700

    httpd: Create listening socket

--- httpd.c
+++ httpd.c
@@ -1,18 +1,31 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <getopt.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 
 static struct context {
 	const char		*progname;
+	int			domain;
+	struct server		*servers;
 	const char		*const opts;
 	const struct option	lopts[];
 } context = {
-	.opts	= "h",
+	.domain	= AF_INET,
+	.opts	= "46h",
 	.lopts	= {
+		{"ipv4",	no_argument,	0,	'4'},
+		{"ipv6",	no_argument,	0,	'6'},
 		{"help",	no_argument,	0,	'h'},
 		{NULL, 0, NULL, 0}, /* sentry */
 	},
+};
+
+struct server {
+	const struct context	*ctx;
+	int			sd;
 };
 
 static void usage(const struct context *restrict ctx, FILE *s, int status)
@@ -23,6 +36,12 @@ static void usage(const struct context *restrict ctx, FILE *s, int status)
 	for (o = ctx->lopts; o->name; o++) {
 		fprintf(s, "\t-%c,--%s:", o->val, o->name);
 		switch (o->val) {
+		case '4':
+			fprintf(s, "\tListen only on IPv4 (default)\n");
+			break;
+		case '6':
+			fprintf(s, "\tListen only on IPv6\n");
+			break;
 		case 'h':
 			fprintf(s, "\tdisplay this message and exit\n");
 			break;
@@ -34,9 +53,41 @@ static void usage(const struct context *restrict ctx, FILE *s, int status)
 	exit(status);
 }
 
-static int init_socket(const struct context *restrict ctx)
+static int init(struct context *ctx)
 {
+	struct server *s;
+	int ret, sd;
+
+	s = malloc(sizeof(struct server));
+	if (s == NULL) {
+		perror("malloc");
+		return -1;
+	}
+	sd = socket(ctx->domain, SOCK_STREAM, 0);
+	if (sd == -1) {
+		perror("socket");
+		ret = -1;
+		goto err;
+	}
+	ctx->servers = s;
+	s->ctx = ctx;
+	s->sd = sd;
 	return 0;
+err:
+	if (s)
+		free(s);
+	return ret;
+}
+
+static void term(struct context *ctx)
+{
+	struct server *s = ctx->servers;
+
+	if (s && s->sd)
+		if (close(s->sd))
+			perror("close");
+	if (s)
+		free(s);
 }
 
 int main(int argc, char *const argv[])
@@ -57,8 +108,9 @@ int main(int argc, char *const argv[])
 			break;
 		}
 	}
-	ret = init_socket(ctx);
+	ret = init(ctx);
 	if (ret == -1)
 		return 1;
+	term(ctx);
 	return 0;
 }
