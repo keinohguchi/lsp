5f296acac4874987945298187495ecbfde1a13f9
Kei Nohguchi <kei@nohguchi.com>
2019-03-30 23:37:10 -0700

    client: Make it a simple shell

--- client.c
+++ client.c
@@ -1,14 +1,25 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <limits.h>
 #include <getopt.h>
 
+struct client {
+	char			buf[LINE_MAX];
+	const struct process	*p;
+};
+
 static struct process {
+	struct client		client[1];		/* single client */
 	const char		*progname;
+	const char		*prompt;
 	const char		*const opts;
 	const struct option	lopts[];
 } process = {
 	.progname	= NULL,
+	.prompt		= "client",
 	.opts		= "h",
 	.lopts		= {
 		{"help",	no_argument,	NULL,	'h'},
@@ -35,10 +46,48 @@ static void usage(const struct process *restrict p, FILE *s, int status)
 	exit(status);
 }
 
+static int is_print_prompt(const struct process *restrict p)
+{
+	return p->prompt[0] != '\0';
+}
+
+static void prompt(const struct process *restrict p)
+{
+	if (is_print_prompt(p))
+		printf("%s> ", p->prompt);
+}
+
+static char *fetch(struct client *ctx)
+{
+	prompt(ctx->p);
+	return fgets(ctx->buf, sizeof(ctx->buf), stdin);
+}
+
+static int handle(struct client *ctx, const char *cmdline)
+{
+	if (!strncasecmp(cmdline, "quit", strlen(cmdline)-1))
+		return 0;
+	printf("%s", cmdline);
+	return 1;
+}
+
+static int init(struct process *p)
+{
+	struct client *c = p->client;
+	c->p = p;
+	return 0;
+}
+
+static void term(const struct process *restrict p)
+{
+	return;
+}
+
 int main(int argc, char *const argv[])
 {
 	struct process *const p = &process;
-	int o;
+	const char *cmd;
+	int o, ret;
 
 	p->progname = argv[0];
 	optind = 0;
@@ -53,5 +102,14 @@ int main(int argc, char *const argv[])
 			break;
 		}
 	}
+	ret = init(p);
+	if (ret == -1)
+		return 1;
+	while ((cmd = fetch(p->client)))
+		if ((ret = handle(p->client, cmd)) <= 0)
+			break;
+	term(p);
+	if (ret)
+		return 1;
 	return 0;
 }
