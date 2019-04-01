12635f35487ddd3379822d33efca0c9b26bf13df
Kei Nohguchi <kei@nohguchi.com>
2019-04-01 12:10:19 -0700

    client: init_address() to parse the address passed through command line

--- client.c
+++ client.c
@@ -6,24 +6,36 @@
 #include <limits.h>
 #include <getopt.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 struct client {
 	char			buf[LINE_MAX];
+	int			sd;
 	const struct process	*p;
 };
 
 static struct process {
 	struct client		client[1];		/* single client */
-	const char		*progname;
 	const char		*prompt;
+	int			port;
+	struct sockaddr_storage	sa;
+	socklen_t		salen;
+	const char		*progname;
 	const char		*const opts;
 	const struct option	lopts[];
 } process = {
+	.client[0].sd	= -1,
 	.progname	= NULL,
 	.prompt		= "client",
+	.port		= 9999,
+	.sa.ss_family	= AF_UNSPEC,
+	.salen		= -1,
 	.opts		= "h",
 	.lopts		= {
-		{"help",	no_argument,	NULL,	'h'},
+		{"help",	no_argument,		NULL,	'h'},
 		{NULL, 0, NULL, 0},
 	},
 };
@@ -31,7 +43,7 @@ static struct process {
 static void usage(const struct process *restrict p, FILE *s, int status)
 {
 	const struct option *o;
-	fprintf(s, "usage: %s [-%s]\n", p->progname, p->opts);
+	fprintf(s, "usage: %s [-%s] <server IP address>\n", p->progname, p->opts);
 	fprintf(s, "options:\n");
 	for (o = p->lopts; o->name; o++) {
 		fprintf(s, "\t-%c,--%s:", o->val, o->name);
@@ -79,9 +91,41 @@ static int handle(struct client *ctx, const char *cmdline)
 	return 1;
 }
 
-static int init(struct process *p)
+static int init_address(struct process *p, const char *addr)
+{
+	unsigned short port = p->port;
+	struct sockaddr_in *sin4;
+	char *colon;
+	int ret;
+
+	colon = strrchr(addr, ':');
+	if (colon != NULL) {
+		int val = strtol(colon, NULL, 10);
+		if (val > 0 && val <= USHRT_MAX) {
+			port = val;
+			*colon = '\0';
+		}
+	}
+	sin4 = (struct sockaddr_in *)&p->sa;
+	ret = inet_pton(AF_INET, addr, &sin4->sin_addr);
+	if (ret == 1) {
+		p->salen = sizeof(struct sockaddr_in);
+		p->sa.ss_family = AF_INET;
+		sin4->sin_port = port;
+		return 0;
+	}
+	/* No IPv6 support yet */
+	return -1;
+}
+
+static int init(struct process *p, char *addr)
 {
 	struct client *c = p->client;
+	int ret;
+
+	ret = init_address(p, addr);
+	if (ret == -1)
+		return -1;
 	c->p = p;
 	return 0;
 }
@@ -111,7 +155,9 @@ int main(int argc, char *const argv[])
 			break;
 		}
 	}
-	ret = init(p);
+	if (optind >= argc)
+		usage(p, stderr, EXIT_FAILURE);
+	ret = init(p, argv[optind]);
 	if (ret == -1)
 		return 1;
 	ctx = p->client;
